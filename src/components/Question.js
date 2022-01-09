f215729099508604216f8d4129eefc1db066cb7e
Kyle Goodwin <kyle.goodwin93@gmail.com>
2022-01-09 18:02:37 -0500

    Done.

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,14 +1,49 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
+
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() => {
+if (timeRemaining!== 0){
+  const intervalId = setTimeout(() => {
+    console.log("running")
+    setTimeRemaining(timeRemaining-1);
+  }, 1000);
+  
+  return function() {
+    console.log("cleanup")
+    clearTimeout(intervalId)
+  }
+}
+else 
+    console.log("timeUp")
+    handleAnswer(false)
+    
+  });
+
+
+  // useEffect(() => {
+  //   if (timeRemaining !== 0){
+  //   const intervalID = setInterval(() => {
+  //     console.log('running')
+  //     setTimeRemaining(timeRemaining - 1)
+  //   }, 1000)}
+  //   else {
+  //     console.log("timeUp")
+  //     // setTimeRemaining(5)
+  //     handleAnswer(false)
+  //   }
+
+    
+  // })
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
     onAnswered(isCorrect);
   }
+
 
   const { id, prompt, answers, correctIndex } = question;
 
